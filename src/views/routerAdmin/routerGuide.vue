81c7635c03a5297a311b75d9e7de1d3ad4de9d32
morebtcg <651932351@qq.com>
2020-11-04 19:33:11 +0800

    split chainAdmin and routerAdmin

---
+++ src/views/routerAdmin/routerGuide.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="app-container">
+    <el-row style="margin-top:20px;">
+      <el-card>
+        <div style="height: 500px;">
+          <el-steps direction="vertical" :active="1">
+            <el-step title="基本信息" description="配置跨链分区、监听端口信息"></el-step>
+            <el-step title="身份服务" description="配置身份服务的IP端口信息"></el-step>
+            <el-step title="下载安装包" description="下载完成的跨链路由安装包"></el-step>
+          </el-steps>
+        </div>
+      </el-card>
+    </el-row>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'RouterGuide',
+  props: {},
+  data() {
+    return {
+      routers: []
+    }
+  },
+  created() {},
+  mounted() {},
+  methods: {}
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>

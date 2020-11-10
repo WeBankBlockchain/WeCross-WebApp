0dcce463f4d025126761ae1d02e7a74497debb25
morebtcg <651932351@qq.com>
2020-11-10 17:22:31 +0800

    improve resource explorer

---
+++ src/views/chainAdmin/ChainManage.vue
@@ -0,0 +1,149 @@
+<template>
+  <div class="app-container">
+    <el-row style="margin-top:20px;">
+      <el-card>
+        <el-row>
+          <el-button-group>
+            <el-button plain icon="el-icon-refresh" @click="refresh">刷新</el-button>
+            <el-button plain icon="el-icon-plus" @click="addChain">添加区块链</el-button>
+          </el-button-group>
+
+          <el-pagination
+background
+:page-size="pageSize"
+pager-count=9
+layout="prev, pager, next"
+:total="total"
+style="float: right;"
+            :current-page="currentPage"
+@prev-click="prevPage"
+@next-click="nextPage"
+@current-change="setPage">
+          </el-pagination>
+        </el-row>
+        <el-row>
+          <el-table ref="singleTable" :data="chains" element-loading-text="加载中..." fit>
+            <el-table-column type="expand">
+              <!--
+              <template slot-scope="item">
+                <el-form label-position="left" class="demo-table-expand">
+
+                  <el-form-item v-for="(propItem, propName) in item.row.properties" v-bind:label="propName">
+                    <span>{{ propItem }}</span>
+                  </el-form-item>
+
+                </el-form>
+              </template>
+              -->
+            </el-table-column>
+            <el-table-column label="区块链路径">
+              <template slot-scope="item">
+                {{ item.row.zone }}.{{ item.row.chain}}
+              </template>
+            </el-table-column>
+            <el-table-column label="类型">
+              <template slot-scope="item">
+                {{ item.row.type }}
+              </template>
+            </el-table-column>
+            <el-table-column label="块高">
+              <template slot-scope="item">
+                {{ item.row.blockNumber }}
+              </template>
+            </el-table-column>
+            <el-table-column label="操作">
+              <template slot-scope="item">
+                <el-button-group>
+                  <el-button plain icon="el-icon-magic-stick">资源浏览</el-button>
+                  <el-button plain icon="el-icon-upload2">部署资源</el-button>
+                  <el-button plain icon="el-icon-minus" @click="removeChain(item.row.zone, item.row.chain)">断开</el-button>
+                </el-button-group>
+              </template>
+            </el-table-column>
+          </el-table>
+        </el-row>
+        <el-row>
+          <el-pagination
+background
+:page-size="pageSize"
+pager-count=9
+layout="prev, pager, next"
+:total="total"
+style="float: right;"
+            :current-page="currentPage"
+@prev-click="prevPage"
+@next-click="nextPage"
+@current-change="setPage">
+          </el-pagination>
+        </el-row>
+      </el-card>
+    </el-row>
+  </div>
+</template>
+
+<script>
+import {
+  listChains
+} from '@/api/conn'
+
+export default {
+  name: 'ChainManage',
+  props: {},
+  data() {
+    return {
+      chains: [],
+      pageSize: 10,
+      total: 0,
+      currentPage: 1
+    }
+  },
+  created() {
+    this.refresh()
+  },
+  mounted() {},
+  methods: {
+    refresh() {
+      listChains({
+        offset: (this.currentPage - 1) * 10,
+        size: this.pageSize
+      }).then(response => {
+        this.chains = response.data.data
+        this.total = response.data.size
+      }).catch(() => {
+        this.$message({
+          type: 'error',
+          message: '刷新失败，网络异常'
+        })
+      })
+    },
+    addChain() {
+      this.$message({
+        type: 'info',
+        message: '该功能尚未支持'
+      })
+    },
+    removeChain(zone, chain) {
+      this.$message({
+        type: 'info',
+        message: '该功能尚未支持'
+      })
+    },
+    nextPage() {
+      ++this.currentPage
+      this.refresh()
+    },
+    prevPage() {
+      --this.currentPage
+      this.refresh()
+    },
+    setPage(page) {
+      this.currentPage = page
+      this.refresh()
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/views/resourceAdmin/explorer.vue
@@ -0,0 +1,110 @@
+<template>
+  <div class="app-container">
+
+    <el-row>
+      <el-card class="box-card">
+        <el-button-group>
+          <el-button icon="el-icon-d-arrow-left" @click="onBack" v-bind:disabled="history.index === 0">后退</el-button>
+          <el-button @click="onForward" v-bind:disabled="history.index + 1 >= history.list.length">前进<i class="el-icon-d-arrow-right"></i></el-button>
+        </el-button-group>
+        <el-button-group style="margin-left: 10px; width:40%">
+          <el-input size="large" placeholder="当前路径" prefix-icon="el-icon-folder" v-model="currentChain" readonly></el-input>
+        </el-button-group>
+        <el-button-group>
+          <el-button icon="el-icon-upload" @click="onDeploy" v-bind:disabled="currentChain === ''">部署资源</el-button>
+        </el-button-group>
+      </el-card>
+    </el-row>
+    <el-row :gutter="10" style="margin-top: 10px">
+      <el-col :span="4">
+        <el-card class="box-card">
+          <div slot="header" class="clearfix">
+            <el-button-group>
+              <el-button icon="el-icon-circle-plus-outline" @click="onAddChain" v-bind:disabled="currentZone === ''">新增区块链</el-button>
+            </el-button-group>
+          </div>
+          <ChainExplorer :chain='currentChain' @zone-click='onZoneClick' @chain-click='onChainClick'></ChainExplorer>
+        </el-card>
+      </el-col>
+      <el-col :span="20">
+        <el-card class="box-card">
+          <ResourceExplorer :chain='currentChain' :pageSize=10></ResourceExplorer>
+        </el-card>
+      </el-col>
+    </el-row>
+
+  </div>
+</template>
+
+<script>
+import ChainExplorer from '@/components/ChainExplorer'
+import ResourceExplorer from '@/components/ResourceExplorer'
+
+export default {
+  name: 'Explorer',
+  components: {
+    ChainExplorer,
+    ResourceExplorer
+  },
+  props: {},
+  data() {
+    return {
+      currentZone: '',
+      currentChain: '',
+      currentChainData: {},
+      searchPath: '',
+      history: {
+        index: 0,
+        list: []
+      }
+    }
+  },
+  created() {},
+  mounted() {},
+  methods: {
+    onZoneClick(path) {
+      if (this.currentZone !== path) {
+        this.currentZone = path
+      }
+    },
+    onChainClick(path, data) {
+      if (this.currentChain !== path) {
+        this.currentChain = path
+        this.currentChainData = data
+        this.currentZone = path.split('.')[0]
+        this.history.list.push(path)
+        this.history.index = this.history.list.length - 1
+      }
+    },
+    onBack() {
+      if (this.history.index > 0) {
+        --this.history.index
+        this.currentChain = this.history.list[this.history.index]
+      }
+    },
+    onForward() {
+      if (this.history.index + 1 < this.history.list.length) {
+        ++this.history.index
+        this.currentChain = this.history.list[this.history.index]
+      }
+    },
+    onDeploy(path, stubType) {
+      console.log('deploy: ' + this.currentChain + ' ' + this.currentChainData.type)
+      this.$router.push({
+        path: 'resourceDeploy',
+        query: {
+          path: this.currentChain,
+          stubType: this.currentChainData.type
+        }
+      })
+    },
+    onAddChain() {
+
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>

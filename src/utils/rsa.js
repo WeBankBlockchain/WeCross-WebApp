7d3eb813460b1bdea647106ff5bd06d5ca63e250
octopus <912554887@qq.com>
2020-11-30 15:38:58 +0800

    add authcode

--- src/utils/rsa.js
+++ src/utils/rsa.js
@@ -1,28 +1,4 @@
 import { JSEncrypt } from 'jsencrypt'
-import { authPub } from '@/api/user'
-import { setPubKey } from '@/utils/auth'
-
-/**
- * query server encrypt publicKey
- */
-export function queryPub(callback) {
-  authPub().then((resp) => {
-    if (
-      typeof resp.errorCode !== 'undefined' &&
-        resp.errorCode !== null &&
-        resp.errorCode !== 0
-    ) {
-      this.$message({
-        type: 'error',
-        message: JSON.stringify(resp)
-      })
-    } else {
-      const pub = resp.data.pub
-      console.log('pub: ' + pub)
-      setPubKey(pub)
-    }
-  })
-}
 
 /**
  * rsa encrypt

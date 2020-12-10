df26a8ff012643bfe093d79461f89dd8b2d18a1d
JimmyShi22 <417711026@qq.com>
2020-12-10 11:21:27 +0800

    add loading icon before auth code loaded

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -56,9 +56,11 @@
         />
       </el-form-item>
       <el-form-item prop="vercode">
-        <div style="width: 100%;height: 10%">
+        <div style="width: 100%;height: 10%;text-align:center;background:white">
           <span>
+            <i v-if="imageAuthCode.imageAuthCodeBase64URL == ''" class="el-icon-loading" />
             <img
+              v-else
               style="width: 100%;height: 10%"
               :src="imageAuthCode.imageAuthCodeBase64URL"
               alt=""

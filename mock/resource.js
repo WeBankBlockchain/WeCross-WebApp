49d42fb33f654c04673dd884636f8328f6448abe
Kyon <32325790+kyonRay@users.noreply.github.com>
2023-06-27 19:34:14 +0800

    <feat>(resource): add deploy wasm feature. (#177)
    * <fix>(resource): remove bcos3 deploy version form.
    
    * <feat>(resource): add deploy wasm feature.
    
    * <fix>(resource): remove bcos3 class version form.

--- mock/resource.js
+++ mock/resource.js
@@ -15,7 +15,7 @@ module.exports = [{
             id: '@id',
             path: config.query.path ? config.query.path + '.@word(3,5)' : '@pick([\'payment\',\'load\',\'resource\']).@pick([\'bcos\',\'bcos_gm\',\'fabric\']).@word(3,5)',
             checksum: 'checksum',
-            'stubType|1': ['BCOS2.0', 'GM_BCOS2.0', 'Fabric1.4'],
+            'stubType|1': ['BCOS2.0', 'GM_BCOS2.0', 'Fabric1.4', 'BCOS3_ECDSA_EVM', 'BCOS3_GM_EVM'],
             properties: '@sentence(3,3)',
             distance: '@integer(0, 3)'
           }]
